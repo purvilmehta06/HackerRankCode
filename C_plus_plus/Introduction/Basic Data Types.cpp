92800b49e0a0a67f763edecbe918bfb72f2e9846
Purvil Mehta <38665244+purvilmehta06@users.noreply.github.com>
2018-07-04 16:20:19 +0530

    Create Basic Data Types.cpp

---
+++ C_plus_plus/Introduction/Basic Data Types.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    int a;
+    long int b;
+    char c;
+    float d;
+    double e;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    cin>>e;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    std :: cout<<std :: setprecision(25);
+    cout<<d<<endl;
+    std :: cout<<std :: setprecision(25);
+    cout<<e<<endl;
+    return 0;
+}
