b6d27bb787cf9923ba761acc790d3eb50c07186a
Purvil Mehta <38665244+purvilmehta06@users.noreply.github.com>
2018-07-04 16:28:02 +0530

    Update and rename C_plus_plus/Vector-Sort.cpp to C_plus_plus/Inheritance/Inheritance Introduction.cpp

---
+++ C_plus_plus/Inheritance/Inheritance Introduction.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+class Triangle{
+    public:
+       void triangle(){
+           cout<<"I am a triangle\n";
+       }
+};
+class sides {
+    public:
+       void description(){
+          cout<<"In an isosceles triangle two sides are equal\n";
+       }
+
+};
+class Isosceles : public Triangle, public sides
+{
+    public:
+       void isosceles(){
+          cout<<"I am an isosceles triangle\n";
+       }
+
+};
+
+int main(){
+    Isosceles isc;
+    isc.isosceles();
+     isc.description();
+    isc.triangle();
+    return 0;
+}
