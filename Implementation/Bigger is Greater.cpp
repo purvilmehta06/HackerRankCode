5ccf26a58d41d1560879db9dbf40cb26d4d1e062
Purvil Mehta <38665244+purvilmehta06@users.noreply.github.com>
2018-07-04 13:55:20 +0530

    Create Bigger is Greater.cpp

---
+++ Implementation/Bigger is Greater.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+int main() {
+
+int t,i;
+
+cin >> t;
+
+string s;
+
+for(i=0;i<t;i++)
+{
+
+ cin >> s;
+
+bool val = next_permutation(s.begin(), s.end());
+
+if (val == false)
+{
+
+    cout << "no answer" << endl;
+
+}
+
+else
+
+{
+
+    cout << s << endl;
+
+}
+}
+
+ return 0;
+}
